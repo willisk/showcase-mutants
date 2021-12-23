c596d60402486d563adbfdbb415c402f1062323a
willisk <kurtwillis@live.com>
2021-12-23 21:09:09 +0100

    tmp

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,12 +2,21 @@ const hre = require('hardhat');
 const { ethers } = require('hardhat');
 
 async function main() {
-  const NFTXXX = await ethers.getContractFactory('NFTXXX');
-  const contract = await NFTXXX.deploy();
+  const NFT = await ethers.getContractFactory('NFT');
+  const MUTANTS = await ethers.getContractFactory('Mutants');
+  const SERUM = await ethers.getContractFactory('Serum');
 
-  await contract.deployed();
+  nft = await NFT.deploy();
+  mutants = await MUTANTS.deploy();
+  serum = await SERUM.deploy();
 
-  console.log('Contract deployed to:', contract.address);
+  await mutants.setSerumAddress(serum.address);
+  await mutants.setNFTAddress(nft.address);
+  await serum.setMutantsAddress(mutants.address);
+
+  console.log('NFT contract deployed to', nft.address);
+  console.log('Mutant contract deployed to', mutants.address);
+  console.log('Serum contract deployed to', serum.address);
 
   // contract.setBaseURI('ipfs://YYY');
 }
